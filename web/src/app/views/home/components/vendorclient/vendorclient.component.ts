b1ca8f159e906859d400be922c7da17be5bf2256
Clement Onawole <dapo.onawole@gmail.com>
2019-09-23 12:05:30 -0600

    more docker build fix

--- web/src/app/views/home/components/vendorclient/vendorclient.component.ts
+++ web/src/app/views/home/components/vendorclient/vendorclient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskCreateComponent } from 'src/app/views/tasks/components/task-create/task-create.component';
 
 @Component({
   selector: 'app-vendorclient',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorclientComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
   }
+
+  openDialog() {
+        const dialogRef = this.dialog.open(TaskCreateComponent, {
+            height: '600px',
+            width: '800px',
+        });
+
+        dialogRef.afterClosed().subscribe(result => { });
+    }
 
 }
