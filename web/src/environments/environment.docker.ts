18b3cade2a5e4300618bb79219886a953fdef4fe
Clement Onawole <dapo.onawole@gmail.com>
2019-09-23 11:51:44 -0600

    fix docker deploy\

--- web/src/environments/environment.docker.ts
+++ web/src/environments/environment.docker.ts
@@ -1,12 +1,13 @@
-const TASK_BASE_URL = 'http://70.77.10.241:8000';
-const AUTH_BASE_URL = 'http://70.77.10.241:8008';
-const MSG_BASE_URL = 'http://70.77.10.241:8004';
+const APIGW_BASE_URL = 'https://sdpro.dynu.com:7000';
 const IP_SERVICE = 'http://api.ipify.org';
 
 export const environment = {
     production: false,
-    TASK_API: `${TASK_BASE_URL}/api`,
-    AUTH_API: `${AUTH_BASE_URL}/api`,
-    MSG_API: `${MSG_BASE_URL}/api`,
-    IP_SERVICE: `${IP_SERVICE}`
+
+    APIGW_API: `${APIGW_BASE_URL}/api`,
+    IP_SERVICE: `${IP_SERVICE}`,
+
+    // AUTH0
+    AUTH0_CLIENTID: 'EKaZmjHFRlGyTS1Fgoxg98KplWV913aK',
+    AUTH0_DOMAIN: 'murpheux.auth0.com',
 };
