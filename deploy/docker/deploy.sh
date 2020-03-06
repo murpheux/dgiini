ea5d6aeb119cedec6f3c4edc6b1896c32a9af551
Clement Onawole <dapo.onawole@gmail.com>
2020-03-06 14:42:43 -0700

    change deployment to local

--- deploy/docker/deploy.sh
+++ deploy/docker/deploy.sh
@@ -4,8 +4,15 @@ currentBuildNumber=$(ls -d release_* | sed -e 's/[^0-9]//g')
 # get latest build number
 latestBuildNumber=$(curl --user murpheux:$JK_TOKEN http://gru:8088/job/dgiini/lastBuild/buildNumber)
 
-CURR_DIR="release_$currentBuildNumber"
+re='^[0-9]+$'
+if ! [[ $currentBuildNumber =~ $re ]] ; then
+    CURR_DIR="release_$latestBuildNumber"
+else
+    CURR_DIR="release_$currentBuildNumber"
+fi
+
 NEW_DIR="release_$latestBuildNumber"
+
 
 if [ -d "$CURR_DIR" ]; then
 
@@ -28,11 +35,13 @@ unzip apigw.zip -d apigw && rm apigw.zip
 unzip mstask.zip -d mstask && rm mstask.zip
 unzip msauth.zip -d msauth && rm msauth.zip
 unzip msmsg.zip -d msmsg && rm msmsg.zip
+unzip msnotify.zip -d msnotify && rm msnotify.zip
+unzip msbill.zip -d msnbill && rm msbill.zip
 unzip web.zip -d web && rm web.zip
 
 cd ../../
 
-sudo chown -R murpheux:murpheux $NEW_DIR
+#sudo chown -R murpheux:murpheux $NEW_DIR
 sudo chmod -R 755 $NEW_DIR
 
 echo 'start new run'
