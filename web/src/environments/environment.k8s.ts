818bfff9cbdf28c26e8f4aae9075594500b33bfa
Clement Onawole <dapo.onawole@gmail.com>
2020-03-11 15:50:39 -0600

    added working mongodb

---
+++ web/src/environments/environment.k8s.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: false,
+
+    gateway: {
+        api: 'https://138.197.236.87/api',
+    },
+
+    // AUTH0
+    auth0: {
+        domain: 'murpheux.auth0.com',
+        clientId: 'EKaZmjHFRlGyTS1Fgoxg98KplWV913aK',
+        callbackURL: 'http://138.197.236.87'
+    },
+
+    version: '0.0.1',
+
+    keys: {
+        gmap: ''
+    }
+};
