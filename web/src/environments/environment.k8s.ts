bebc05f4bc5bd71a93352932efbce76e2fe6be97
Clement Onawole <dapo.onawole@gmail.com>
2020-03-15 18:30:46 -0600

    updated config to https for web

--- web/src/environments/environment.k8s.ts
+++ web/src/environments/environment.k8s.ts
@@ -2,14 +2,14 @@ export const environment = {
     production: false,
 
     gateway: {
-        api: 'https://138.197.236.87/api',
+        api: 'https://dgiini-apigw-service/api',
     },
 
     // AUTH0
     auth0: {
         domain: 'murpheux.auth0.com',
         clientId: 'EKaZmjHFRlGyTS1Fgoxg98KplWV913aK',
-        callbackURL: 'http://138.197.236.87'
+        callbackURL: 'https://dgiini-web-service'
     },
 
     version: '0.0.1',
