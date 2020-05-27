e26a4d04a39250c8d9b7224e9e636ff3459f2887
Clement Onawole <dapo.onawole@gmail.com>
2020-05-26 19:32:23 -0600

    changes made for build to pass

--- ms/src/version.js
+++ ms/src/version.js
@@ -1,10 +1,10 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    'dirty': true,
-    'raw': 'v0.0.1-85-gc7fec38b-dirty',
-    'hash': 'gc7fec38b',
-    'distance': 85,
+    'dirty': false,
+    'raw': 'v0.0.1-102-g0b628ceb',
+    'hash': 'g0b628ceb',
+    'distance': 102,
     'tag': 'v0.0.1',
     'semver': {
         'options': {
@@ -20,8 +20,8 @@ export const VERSION = {
         'build': [],
         'version': '0.0.1'
     },
-    'suffix': '85-gc7fec38b-dirty',
-    'semverString': '0.0.1+85.gc7fec38b',
+    'suffix': '102-g0b628ceb',
+    'semverString': '0.0.1+102.g0b628ceb',
     'version': '1.0.0'
 }
 /* tslint:enable */
