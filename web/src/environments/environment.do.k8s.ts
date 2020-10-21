59147984cdd0b34b511cd910d4a9d7a0bd001bad
Clement Onawole <dapo.onawole@gmail.com>
2020-10-20 18:34:59 -0600

    ref to backend issue

---
+++ web/src/environments/environment.do.k8s.ts
@@ -0,0 +1,29 @@
+export const environment = {
+    production: false,
+
+    // AUTH0
+    auth0: {
+        domain: 'murpheux.auth0.com',
+        clientId: 'CMVPqSIb_wIkCr6gfdCk2q9JPDJh41CW',
+        callbackURL: 'https://gw.dgiini.ca',
+    },
+
+    googleApi: {
+        API_KEY: 'AIzaSyB4rkOg7uSupskMTi-2kiteuvn2KaBswZU'
+    },
+
+    provider: {
+        ip: 'http://api.ipify.org/?format=json',
+        proxy: 'https://cors-anywhere.herokuapp.com',
+    },
+
+    stripe: {
+        Publishable_key: 'pk_test_51HRj2GEoqCmvHb0cQ99bpnIBGAS5jsbM1pakIjItoVtrgkIghwkGEmmEt7Yh14YRvHc9LE3LPePNobbQ28DneqiE008QNBThEh',
+        Secret_key: 'sk_test_51HRj2GEoqCmvHb0c5e8Tgg7CRGDpgULvw2O02tajBt83jLhJTPSOVx6J3EXEUiqHurBFCOgAGj0Rm35LHqF315Ky00gBxqVcf1'
+    },
+
+    countries: [
+        { name: 'Canada', code: 'ca' },
+        { name: 'United States', code: 'us' },
+    ],
+};
